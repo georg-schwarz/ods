4a4fbbff7b391cd984a45733667e2ca9ce5be95c
Georg Schwarz <schwarz@group.riehle.org>
2021-11-17 17:59:30 +0100

    Provider test for first scenario /configs?pipelineId=5

---
+++ notification/src/ui.provider.pact.test.ts
@@ -0,0 +1,153 @@
+import path from 'path';
+
+import { Verifier } from '@pact-foundation/pact';
+
+import {
+  NotificationConfig,
+  NotificationType,
+} from './notification-config/notificationConfig';
+import { NotificationRepository } from './notification-config/notificationRepository';
+import { PostgresNotificationRepository } from './notification-config/postgresNotificationRepository';
+
+import { port, server } from './index'; // The main method is automatically called due to this import
+
+const notificationConfigs: NotificationConfig[] = [];
+let nextNotificationConfigId = 0;
+
+jest.mock('./notification-config/postgresNotificationRepository', () => {
+  return {
+    initNotificationRepository: jest
+      .fn()
+      .mockImplementation((): Promise<NotificationRepository> => {
+        return Promise.resolve(new PostgresNotificationRepository());
+      }),
+    PostgresNotificationRepository: jest.fn().mockImplementation(() => {
+      return {
+        init: jest.fn().mockImplementation((): Promise<void> => {
+          return Promise.resolve();
+        }),
+        getAll: jest.fn().mockImplementation(() => notificationConfigs),
+        getForPipeline: jest
+          .fn()
+          .mockImplementation(async (pipelineId: number) => {
+            const result = notificationConfigs.filter(
+              (config) => config.pipelineId === pipelineId,
+            );
+            return Promise.resolve(result);
+          }),
+
+        getById: jest.fn().mockImplementation(async (id: number) => {
+          const result = notificationConfigs.find((config) => config.id === id);
+          return Promise.resolve(result);
+        }),
+
+        create: jest
+          .fn()
+          .mockImplementation(async (config: NotificationConfig) => {
+            const result: NotificationConfig = {
+              ...config,
+              id: ++nextNotificationConfigId,
+            };
+            notificationConfigs.push(result);
+            return await Promise.resolve(result);
+          }),
+
+        update: jest.fn(
+          async (
+            id: number,
+            config: NotificationConfig,
+          ): Promise<NotificationConfig> => {
+            const configToUpdate = notificationConfigs.find(
+              (config) => config.id === id,
+            );
+            return Promise.resolve(Object.assign(configToUpdate, config));
+          },
+        ),
+
+        delete: jest.fn((id: number): Promise<void> => {
+          const indexOfConfigToDelete = notificationConfigs.findIndex(
+            (config) => config.id === id,
+          );
+          if (indexOfConfigToDelete !== -1) {
+            notificationConfigs.splice(indexOfConfigToDelete, 1);
+          }
+          return Promise.resolve();
+        }),
+      };
+    }),
+  };
+});
+
+// The following mocks are needed for propper execution of the main function
+jest.mock('./notification-execution/notificationExecutor', () => {
+  return function (): unknown {
+    return {};
+  };
+});
+jest.mock('./api/amqp/pipelineSuccessConsumer', () => {
+  return {
+    createPipelineSuccessConsumer: jest.fn(),
+  };
+});
+jest.mock('@jvalue/node-dry-amqp', () => {
+  return {
+    AmqpConnection: jest.fn(),
+  };
+});
+
+describe('Pact Provider Verification', () => {
+  it('validates the expectations of the UI', async () => {
+    const verifier = new Verifier({
+      provider: 'Notification',
+      providerBaseUrl: `http://localhost:${port}`,
+      pactUrls: [
+        path.resolve(process.cwd(), '..', 'pacts', 'ui-notification.json'),
+      ],
+      logDir: path.resolve(process.cwd(), '..', 'pacts', 'logs'),
+      stateHandlers: {
+        'some notifications exist for pipeline 5': setupSomeNotificationConfigs,
+        'no notifications exist for pipeline 5': setupEmptyState,
+      },
+    });
+    await verifier.verifyProvider().finally(() => {
+      server?.close();
+    });
+  });
+});
+
+async function setupEmptyState(): Promise<void> {
+  clearState();
+
+  return Promise.resolve();
+}
+
+async function setupSomeNotificationConfigs(): Promise<void> {
+  clearState();
+  addSampleNotificationConfig(++nextNotificationConfigId, 2);
+  addSampleNotificationConfig(++nextNotificationConfigId, 3);
+  addSampleNotificationConfig(++nextNotificationConfigId, 5);
+
+  return Promise.resolve();
+}
+
+function clearState(): void {
+  nextNotificationConfigId = 0;
+  clearNotificationConfigs();
+}
+
+function clearNotificationConfigs(): void {
+  notificationConfigs.splice(0, notificationConfigs.length);
+}
+
+function addSampleNotificationConfig(id: number, pipelineId: number): void {
+  const notificationConfig: NotificationConfig = {
+    id: id,
+    pipelineId: pipelineId,
+    type: NotificationType.WEBHOOK,
+    parameter: {
+      url: 'http://www.example-data.com',
+    },
+    condition: 'true',
+  };
+  notificationConfigs.push(notificationConfig);
+}
